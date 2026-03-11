eccc037b80ac03e9fe234f5886d6d961f2b1dabd
mygithub1981 <151316180+mygithub1981@users.noreply.github.com>
2026-03-11 23:44:17 +0800

    Clean up feed source entries in opdiy-part1.sh
    Removed multiple echo commands for feed sources.

--- opdiy-part1.sh
+++ opdiy-part1.sh
@@ -1,6 +1,1 @@
 echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >>feeds.conf.default
-echo 'src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
-echo 'src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go.git;main' >>feeds.conf.default
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git;main' >>feeds.conf.default
