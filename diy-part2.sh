09679031d3d19e55270e4df8e891f15b3242c81a
mygithub1981 <151316180+mygithub1981@users.noreply.github.com>
2026-04-04 15:10:55 +0800

    Remove ksmbd configurations from diy-part2.sh
    Removed ksmbd related configurations and dependencies from the script.

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,16 +21,3 @@
 # 修改默认 IP 为 192.168.50.250
 # 自动查找 config_generate 文件并修改默认 IP
 find package/base-files/ -name config_generate -exec sed -i 's/192.168.1.1/192.168.50.252/g' {} +
-# 1. 修改源码层面的依赖，把默认的 ksmbd 替换为 samba4
-sed -i 's/luci-app-ksmbd/luci-app-samba4/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-app-ksmbd/luci-app-samba4/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-app-ksmbd/luci-app-samba4/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-
-# 2. 强行删除 .config 文件中所有与 ksmbd 相关的残留配置
-sed -i '/CONFIG_PACKAGE_luci-app-ksmbd=y/d' .config
-sed -i '/CONFIG_PACKAGE_ksmbd-server=y/d' .config
-sed -i '/CONFIG_PACKAGE_kmod-fs-ksmbd=y/d' .config
-
-# 3. 确保 .config 文件中写入了 samba4 及其依赖（可选，作为双保险）
-echo "CONFIG_PACKAGE_luci-app-samba4=y" >> .config
-echo "CONFIG_PACKAGE_autosamba=y" >> .config
